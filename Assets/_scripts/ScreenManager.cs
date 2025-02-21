d29868bfbb157275175995d6093d72cb3a0cc24e
Jewel <jewelxjocelyn@gmail.com>
2025-02-20 17:04:44 -0800

    update screenmanager

--- Assets/_scripts/ScreenManager.cs
+++ Assets/_scripts/ScreenManager.cs
@@ -18,6 +18,7 @@ public class ScreenManager : MonoBehaviour
     //switches scenes
     public void SwitchScene(int buildIndex)
     {
+        Time.timeScale = 1.0f;
         SceneManager.LoadScene(buildIndex);
 
     }
