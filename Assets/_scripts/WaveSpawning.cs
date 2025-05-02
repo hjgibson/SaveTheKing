d197fdbf3d0897064dacc5758413817bfd05fc3d
hjgibson <148888831+hjgibson@users.noreply.github.com>
2025-05-01 21:53:20 -0700

    wave spawning fix commented

--- Assets/_scripts/WaveSpawning.cs
+++ Assets/_scripts/WaveSpawning.cs
@@ -82,7 +82,8 @@ public class WaveSpawning : MonoBehaviour
     {
         buildPhasePanel.SetActive(true); // Show the build phase panel
         waveText.text = "Wave: " + (waveNumber + 1);
-        Time.timeScale = 0f;
+        Time.timeScale = 0f; // can change to 1 to allow merge to work during build pahse, but it can work endlessly
+        
     }
 
     public void StartNextWave()
